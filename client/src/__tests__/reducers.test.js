24c0070e9aa2fd2fdeab42cd223cf0417443b30b
Rahil Rahimi <rahilrahimi1362@gmail.com>
2022-05-03 18:28:16 -0400

    initial change

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -4,6 +4,7 @@ import {
     UPDATE_CATEGORIES,
     UPDATE_CURRENT_CATEGORY
   } from '../utils/actions';
+  import { reducer } from '../utils/reducers';
   
   // create a sample of what our global state will look like
   const initialState = {
@@ -22,3 +23,22 @@ import {
     expect(initialState.products.length).toBe(0);
   });
 
+  test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CATEGORIES,
+      categories: [{}, {}]
+    });
+  
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+  });
+  
+  test('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: '2'
+    });
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+  });
