dd59b8a6d749247a1973845cad254372917292bd
Rahil Rahimi <rahilrahimi1362@gmail.com>
2022-05-19 16:42:10 -0400

    modified client/src/utils/actions.js and __tests__/reducers.test.js

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -2,7 +2,13 @@
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
   } from '../utils/actions';
   import { reducer } from '../utils/reducers';
   
@@ -11,6 +17,19 @@ import {
     products: [],
     categories: [{ name: 'Food' }],
     currentCategory: '1',
+    cart: [
+      {
+        _id: '1',
+        name: 'Soup',
+        purchaseQuantity: 1
+      },
+      {
+        _id: '2',
+        name: 'Bread',
+        purchaseQuantity: 2
+      }
+    ],
+    cartOpen: false
   };
 
   test('UPDATE_PRODUCTS', () => {
